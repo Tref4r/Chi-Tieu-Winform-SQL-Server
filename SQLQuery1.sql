9dbaaf820a94e11302f6eeecae69a8cab92f8129
Tref4r <113423456+Tref4r@users.noreply.github.com>
2024-04-01 09:07:57 +0700

    Update SQLQuery1.sql to fix ADMIN side bug

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,9 +1,10 @@
-﻿Create Database DA_QLCT
-
+-- Tạo Database DA_QLCT
+Create Database DA_QLCT
+-- Sử dụng database DA_QLCT
 Use DA_QLCT
 
-// Tạo table tài khoản
 
+-- Tạo bảng Tài Khoản
 CREATE TABLE TaiKhoan
 (
     TenDangNhap NVARCHAR(50) PRIMARY KEY,
@@ -15,12 +16,14 @@ CREATE TABLE TaiKhoan
     DiaChi NVARCHAR(100),
     LoaiTaiKhoan NVARCHAR(50)
 )
-Select * From TaiKhoan
 
+-- Cập nhật loại tài khoản thành ADMIN cho người dùng 'Tref'
 UPDATE TaiKhoan
 SET LoaiTaiKhoan = 'ADMIN'
 WHERE TenDangNhap = 'Tref';
-//
+
+
+-- Tạo stored procedure để kiểm tra tài khoản
 CREATE PROCEDURE CheckTaiKhoan
     @taikhoan NVARCHAR(50),
     @matkhau NVARCHAR(50),
@@ -31,7 +34,7 @@ BEGIN
     WHERE TenDangNhap = @taikhoan AND MatKhau = @matkhau AND LoaiTaiKhoan = @loai;
 END
 
-//
+-- Tạo stored procedure để thay đổi mật khẩu của tài khoản
 CREATE PROCEDURE DoiMatKhauTaiKhoan
     @taikhoan NVARCHAR(50),
     @matkhaumoi NVARCHAR(50)
@@ -46,8 +49,8 @@ BEGIN
     ELSE
         SELECT 0 AS Result
 END
-//
 
+-- Tạo stored procedure để cập nhật thông tin của tài khoản
 CREATE PROCEDURE CapNhatTaiKhoan
     @taikhoan NVARCHAR(50),
     @hoten NVARCHAR(50),
@@ -67,7 +70,7 @@ BEGIN
         SELECT 0 AS Result
 END
 
-//
+-- Tạo stored procedure để xóa tài khoản
 CREATE PROCEDURE XoaTaiKhoan
     @taikhoan NVARCHAR(50)
 AS
@@ -81,8 +84,8 @@ BEGIN
         SELECT 0 AS Result
 END
 
-//
 
+-- Tạo stored procedure để thêm tài khoản
 CREATE PROCEDURE ThemTaiKhoan
     @taikhoan NVARCHAR(50),
     @matkhau NVARCHAR(50),
@@ -100,19 +103,22 @@ BEGIN
     RETURN @@ROWCOUNT;
 END
 
-//
+-- Tạo bảng Phương Thức Thanh Toán
 CREATE TABLE PhuongThucThanhToan
 (
     MaPhuongThucThanhToan NVARCHAR(50) PRIMARY KEY,
     TenPhuongThucThanhToan NVARCHAR(50)
 )
-drop table PhuongThucThanhToan
+
+-- Tạo bảng Danh Mục Thu
 CREATE TABLE DanhMucThu
 (
     MaDanhMuc NVARCHAR(50) PRIMARY KEY,
     TenDanhMuc NVARCHAR(50)
 )
-//
+
+
+-- Thêm dữ liệu vào bảng Danh Mục Thu
 INSERT INTO DanhMucThu (MaDanhMuc, TenDanhMuc)
 VALUES 
 ('DM01', N'Lương'), 
@@ -120,21 +126,25 @@ VALUES
 ('DM03', N'Tiền cho thuê nhà'),
 ('DM04', N'Bán hàng'),
 ('DM05', N'Đầu tư');
-//
+
+
+-- Thêm dữ liệu vào bảng PhuongThucThanhToan
 INSERT INTO PhuongThucThanhToan (MaPhuongThucThanhToan, TenPhuongThucThanhToan)
 VALUES 
 ('PT01', N'Tiền mặt'), 
 ('PT02', N'Chuyển khoản'),
+('PT04`', N'Dạng háng'),
 ('PT03', N'Thẻ');
+-- Lấy dữ liệu từ bảng Danh Mục Thu
 select* from DanhMucThu
+-- Lấy dữ liệu từ bảng PhuongThucThanhToan
 select*from PhuongThucThanhToan
-//
 
-DELETE FROM DanhMucThu WHERE TenDanhMuc = N'Bán hàng';
-DELETE FROM DanhMucThu WHERE TenDanhMuc = N'Tiền cho thuê nhà';
 
-//
 
+
+
+-- Tạo bảng Chi Tiết Giao Dịch Thu
 CREATE TABLE CTGiaoDichThu
 (
     MaGiaoDich INT IDENTITY(1,1) PRIMARY KEY,
@@ -145,8 +155,8 @@ CREATE TABLE CTGiaoDichThu
     MoTa NVARCHAR(MAX),
     NgayGiaoDich DATE
 )
-//
 
+-- Tạo stored procedure để cập nhật giao dịch thu
 CREATE PROCEDURE CapNhatGiaoDichThu
     @magiaodich NVARCHAR(50),
     @taikhoan NVARCHAR(50),
@@ -162,9 +172,9 @@ BEGIN
     WHERE MaGiaoDich = @magiaodich;
 END
 
-//
 
 
+-- Tạo stored procedure để thêm giao dịch thu
 CREATE PROCEDURE ThemGiaoDichThu
     @taikhoan NVARCHAR(50),
     @madanhmuc NVARCHAR(50),
@@ -178,8 +188,8 @@ BEGIN
     VALUES (@taikhoan, @madanhmuc, @maphuongthucthanhtoan, @sotien, @mota, @ngaygiaodich);
 END
 
-//
 
+-- Lấy dữ liệu từ bảng CTGiaoDichThu
 SELECT CTGiaoDichThu.MaGiaoDich, TenDangNhap, CTGiaoDichThu.MaDanhMuc,TenDanhMuc, SoTien, MoTa, NgayGiaoDich,
 CTGiaoDichThu.MaPhuongThucThanhToan , TenPhuongThucThanhToan 
 FROM CTGiaoDichThu 
@@ -187,48 +197,56 @@ INNER JOIN DanhMucThu ON CTGiaoDichThu.MaDanhMuc = DanhMucThu.MaDanhMuc
 INNER JOIN PhuongThucThanhToan ON CTGiaoDichThu.MaPhuongThucThanhToan = PhuongThucThanhToan.MaPhuongThucThanhToan;
 
 
-//
 
+-- Tạo bảng Danh Mục Chi
 CREATE TABLE DanhMucChi
 (
     MaDanhMuc NVARCHAR(50) PRIMARY KEY,
     TenDanhMuc NVARCHAR(50)
 )
-drop table DanhMucChi
-drop table CTGiaoDichChi
+
+
+-- Thêm dữ liệu vào bảng
 INSERT INTO DanhMucChi(MaDanhMuc, TenDanhMuc)
 VALUES 
 ('DM01', N'Yêu đương'), 
 ('DM02', N'Ăn chơi'), 
 ('DM03', N'Chăm sóc sức khỏe'),
 ('DM04', N'Mua sắm'),
-('DM05', N'Đầu tư');
-//
+('DM05', N'Đầu tư'),
+('DM06', N'Trả nợ');
 
+
+-- Lấy dữ liệu từ bảng
 select* from DanhMucChi
 select*from PhuongThucThanhToan
-DROP TABle CTGiaoDichThu
-dROP TABLE PhuongThucThanhToan
-DROP TABLE DanhMucThu
 
-//
 
+
+-- Tạo bảng CTGiaoDichChi
 CREATE TABLE CTGiaoDichChi
 (
     MaGiaoDich INT IDENTITY(1,1) PRIMARY KEY,
-    TenDangNhap NVARCHAR(50) FOREIGN KEY REFERENCES TaiKhoan(TenDangNhap),
+    TenDangNhap NVARCHAR(50),
     MaDanhMuc NVARCHAR(50) FOREIGN KEY REFERENCES DanhMucChi(MaDanhMuc),
     MaPhuongThucThanhToan NVARCHAR(50) FOREIGN KEY REFERENCES PhuongThucThanhToan(MaPhuongThucThanhToan),
     SoTien DECIMAL(18, 2),
     MoTa NVARCHAR(MAX),
-    NgayGiaoDich DATE
+    NgayGiaoDich DATE,
+    FOREIGN KEY (TenDangNhap) REFERENCES TaiKhoan(TenDangNhap) ON DELETE CASCADE
 )
 
 
-//
+ALTER TABLE CTGiaoDichChi
+DROP CONSTRAINT FK__CTGiaoDic__TenDa__1332DBDC
+
+ALTER TABLE CTGiaoDichChi
+ADD CONSTRAINT FK__CTGiaoDic__TenDa__1332DBDC FOREIGN KEY (TenDangNhap)
+REFERENCES TaiKhoan(TenDangNhap) ON DELETE CASCADE
 
 
-//GiaoDichChi
+
+-- Tạo stored procedure để cập nhật giao dịch chi
 CREATE PROCEDURE CapNhatGiaoDichChi
     @magiaodich NVARCHAR(50),
     @taikhoan NVARCHAR(50),
@@ -248,7 +266,10 @@ BEGIN
         NgayGiaoDich = @ngaygiaodich
     WHERE MaGiaoDich = @magiaodich;
 END
-//
+
+
+
+-- Tạo stored procedure để thêm giao dịch chi
 CREATE PROCEDURE ThemGiaoDichChi
     @taikhoan NVARCHAR(50),
     @madanhmuc NVARCHAR(50),
@@ -261,20 +282,21 @@ BEGIN
     INSERT INTO CTGiaoDichChi(TenDangNhap, MaDanhMuc, MaPhuongThucThanhToan, SoTien, MoTa, NgayGiaoDich)
     VALUES (@taikhoan, @madanhmuc, @maphuongthucthanhtoan, @sotien, @mota, @ngaygiaodich);
 END
-//
 
+
+
+-- Lấy dữ liệu từ bảng CTGiaoDichChi
 SELECT CTGiaoDichChi.MaGiaoDich, TenDangNhap, CTGiaoDichChi.MaDanhMuc, TenDanhMuc, SoTien, MoTa, NgayGiaoDich,
 CTGiaoDichChi.MaPhuongThucThanhToan , TenPhuongThucThanhToan 
 FROM CTGiaoDichChi 
 INNER JOIN DanhMucChi ON CTGiaoDichChi.MaDanhMuc = DanhMucChi.MaDanhMuc 
 INNER JOIN PhuongThucThanhToan ON CTGiaoDichChi.MaPhuongThucThanhToan = PhuongThucThanhToan.MaPhuongThucThanhToan;
 
-//
 
 
 
 
-
+-- Lấy dữ liệu từ bảng Tài Khoản
 select *from TaiKhoan
 
 
